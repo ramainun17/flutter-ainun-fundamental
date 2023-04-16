c19fc6fd6f2dce6a5fc4e3db011895da23efdf66
ramainun17 <zenaken107@gmail.com>
2023-04-16 23:13:30 +0700

    update ui

--- lib/main.dart
+++ lib/main.dart
@@ -15,60 +15,110 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController controller1 = TextEditingController();
-  TextEditingController controller2 = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculator"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Text("Result : 0",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: controller1,
-              decoration: InputDecoration(
-                  labelText: "Enter First Number",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: controller2,
-              decoration: InputDecoration(
-                  labelText: "Enter Second Number",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(onPressed: () {}, child: Text("Add")),
-                ElevatedButton(onPressed: () {}, child: Text("Substract")),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(onPressed: () {}, child: Text("Multiply")),
-                ElevatedButton(onPressed: () {}, child: Text("Divide")),
-              ],
-            ),
-          ],
+  Widget buildButton(
+      String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0.0),
+          side: BorderSide(
+              color: Colors.white, width: 1, style: BorderStyle.solid),
+        ),
+        // padding: EdgeInsets.all(16.0),
+        onPressed: null,
+        child: Text(
+          buttonText,
+          style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.normal,
+              color: Colors.white),
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Calculator")),
+        body: Column(
+          children: [
+            Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                child: Text(
+                  "0",
+                  style: TextStyle(fontSize: 38.0),
+                )),
+            Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                child: Text(
+                  "0",
+                  style: TextStyle(fontSize: 48.0),
+                )),
+            Expanded(
+              child: Divider(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  child: Table(
+                    children: [
+                      TableRow(children: [
+                        buildButton("C", 1, Colors.redAccent),
+                        buildButton("Del", 1, Colors.blue),
+                        buildButton("÷", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("7", 1, Colors.black54),
+                        buildButton("8", 1, Colors.black54),
+                        buildButton("9", 1, Colors.black54),
+                      ]),
+                      TableRow(children: [
+                        buildButton("4", 1, Colors.black54),
+                        buildButton("5", 1, Colors.black54),
+                        buildButton("6", 1, Colors.black54),
+                      ]),
+                      TableRow(children: [
+                        buildButton("1", 1, Colors.black54),
+                        buildButton("2", 1, Colors.black54),
+                        buildButton("3", 1, Colors.black54),
+                      ]),
+                      TableRow(children: [
+                        buildButton(".", 1, Colors.black54),
+                        buildButton("0", 1, Colors.black54),
+                        buildButton("00", 1, Colors.black54),
+                      ]),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  child: Table(
+                    children: [
+                      TableRow(children: [
+                        buildButton("×", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("-", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("+", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("=", 2, Colors.redAccent),
+                      ]),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
